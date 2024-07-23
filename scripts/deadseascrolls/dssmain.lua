59ca05a2bc4a7f747312d020c1220672c415573f
CVS-otherwise-Callie <notes2noah@gmail.com>
2024-07-23 07:13:05 -0400

    Update dssmain.lua

--- scripts/deadseascrolls/dssmain.lua
+++ scripts/deadseascrolls/dssmain.lua
@@ -81,19 +81,26 @@ end
 function MenuProvider.SaveMenusPoppedUp(var)
 	CutMonsterPack.savedata.MenusPoppedUp = var
 end
-local DSSInitializerFunction = include("scripts.iam.deadseascrolls.dssmenucore")
+local DSSInitializerFunction = include("scripts.deadseascrolls.dssmenucore")
 local dssmod = DSSInitializerFunction(DSSModName, DSSCoreVersion, MenuProvider)
 
 
 local restoreddirectory = {
     main = {
-        title = 'restored monster pack',
+        title = 'restored monsters',
 
         buttons = {
             {str = 'resume game', action = 'resume'},
             {str = 'settings', dest = 'settings',tooltip = {strset = {'---','play around', 'with what', 'you like and', 'do not like', '---'}}},
             dssmod.changelogsButton,
-            {str = '', fsize=2, nosel = true},
+            {str = '', nosel = true},
+            {str = 'restored monster pack:',fsize=2, nosel = true},
+            {str = 'bringing back your', fsize=2,nosel = true},
+            {str = 'favorite foes since 2023', fsize=2,nosel = true},
+            {str = '', fsize=2,nosel = true},
+            {str = 'play us with', fsize=2,nosel = true},
+            {str = 'fiend folio, the future', fsize=2,nosel = true},
+            {str = 'revelations, and more!', fsize=2,nosel = true},
         },
         tooltip = dssmod.menuOpenToolTip,
     },
