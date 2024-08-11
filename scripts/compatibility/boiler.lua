3eaac3b654296b7ce1264eec976f5f41ae24f090
Goganidze <117683466+Goganidze@users.noreply.github.com>
2024-08-11 22:40:17 +0500

    my bad

--- scripts/compatibility/boiler.lua
+++ scripts/compatibility/boiler.lua
@@ -1,12 +1,8 @@
 if FFGRACE then
     local basepath = "gfx/monsters/"
 
-    FFGRACE.StageSkins = {
-        Boiler = {
-            [EntityType.ENTITY_DUMPLING.." "..0] = { --Frowning Gaper
+    FFGRACE.StageSkins.Boiler[EntityType.ENTITY_DUMPLING.." "..0] = {
                 {{0}, basepath.."boiler/800.000_dumpling"},
-            },
-        }
-    }
+            }
 
 end
