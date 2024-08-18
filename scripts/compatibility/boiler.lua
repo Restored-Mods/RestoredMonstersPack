aed32cb751774bfbb98b0cb9359d3353d2d7c32e
NaturalTree <norbizarzy@gmail.com>
2024-08-18 13:36:08 +0200

    Boiler rooms implementation

--- scripts/compatibility/boiler.lua
+++ scripts/compatibility/boiler.lua
@@ -4,5 +4,13 @@ if FFGRACE then
     FFGRACE.StageSkins.Boiler[EntityType.ENTITY_DUMPLING.." "..0] = {
                 {{0}, basepath.."boiler/800.000_dumpling"},
             }
-
+            
+    table.insert(FFGRACE.Rooms.Boiler, include("resources.luarooms.ffg.boiler_tc"))
+    table.insert(FFGRACE.Rooms.BoilerChallenge, include("resources.luarooms.ffg.boiler_tc_challenge"))
+    table.insert(FFGRACE.Rooms.BoilerWhiteFire, include("resources.luarooms.ffg.boiler_tc_fire"))
+    if FiendFolio then
+      table.insert(FFGRACE.Rooms.Boiler, include("resources.luarooms.ffg.boiler_tc_ff"))
+    end
+    
+    StageAPI.GetBossData("FFGRACE Creem").Rooms:AddRooms(require("resources.luarooms.ffg.bosses.boiler_tc_creem"))
 end
