66674d67aa19bee987691dafe1990d815a73395c
An-Annoying-Cat <lolcats.fun@gmail.com>
2024-09-07 22:17:02 -0500

    added genders to sporeling and chubby bunny

--- scripts/compatibility/fiend folio/tc_genders.lua
+++ scripts/compatibility/fiend folio/tc_genders.lua
@@ -26,8 +26,9 @@ RestoredMonsterPack.Nonmale = {
 	{ID = mod:SpecialEnt("Strifer"), Affliction = "Woman"},
 	{ID = mod:SpecialEnt("Vessel (Antibirth)"), Affliction = "Woman"},
 	{ID = mod:SpecialEnt("Gilded Dumpling"), Affliction = "Gay"}, --bc pandora said they're gay as actual fuck
-	-- {ID = mod:SpecialEnt("Sporeling"), Affliction = "Ace"}, uncomment this when its are added to entities2.lua
-	-- {ID = mod:SpecialEnt("Chubby Bunny"), Affliction = "Trans"},  uncomment this when its are added to entities2.lua
+	{ID = mod:SpecialEnt("Sporeling"), Affliction = "Ace"},
+	{ID = mod:SpecialEnt("Chubby Bunny"), Affliction = "Trans"},
+	-- {ID = mod:SpecialEnt("Beard Bat"), Affliction = "Non-Binary"},
 }
 
 function mod:MixTables(input, table)
