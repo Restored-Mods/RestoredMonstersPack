d27b31e277ff0935fd4df96b61d64d84c3f20b13
We Strvn <diegomonge1@outlook.com>
2025-03-02 00:27:46 -0500

    Changed some function and file names

--- scripts/deadseascrolls/imgui.lua
+++ scripts/deadseascrolls/imgui.lua
@@ -2,50 +2,50 @@ if not REPENTOGON then return end
 local mod = RestoredMonsterPack
 
 -- ImGui menu
-if not ImGui.ElementExists("tcMods") then
-    ImGui.CreateMenu("tcMods", "TC Mods")
+if not ImGui.ElementExists("RestoredMods") then
+    ImGui.CreateMenu("RestoredMods", "Restored Mods")
 end
 
 -- Restored Monsters menu
-if not ImGui.ElementExists("tcRestoredMonsters") then
-    ImGui.AddElement("tcMods", "tcRestoredMonsters", ImGuiElement.MenuItem, "Restored Monsters")
+if not ImGui.ElementExists("RRestoredMonsters") then
+    ImGui.AddElement("RestoredMods", "RRestoredMonsters", ImGuiElement.MenuItem, "Restored Monsters")
 end
 
 -- Restored Monsters window
-if not ImGui.ElementExists("tcRestoredMonstersWindow") then
-    ImGui.CreateWindow("tcRestoredMonstersWindow", "Restored Monsters")
+if not ImGui.ElementExists("RRestoredMonstersWindow") then
+    ImGui.CreateWindow("RRestoredMonstersWindow", "Restored Monsters")
 end
-ImGui.LinkWindowToElement("tcRestoredMonstersWindow", "tcRestoredMonsters")
+ImGui.LinkWindowToElement("RRestoredMonstersWindow", "RRestoredMonsters")
 
 -- Check for existing tab bar
-if ImGui.ElementExists("tcRestoredMonstersTabs") then
-    ImGui.RemoveElement("tcRestoredMonstersTabs")
+if ImGui.ElementExists("RRestoredMonstersTabs") then
+    ImGui.RemoveElement("RRestoredMonstersTabs")
 end
 
 -- Restred Monsters tab bar
-ImGui.AddTabBar("tcRestoredMonstersWindow", "tcRestoredMonstersTabs")
+ImGui.AddTabBar("RRestoredMonstersWindow", "RRestoredMonstersTabs")
 
 -- Vessels tab
-ImGui.AddTab("tcRestoredMonstersTabs", "tcRestoredMonstersTabVessel", "Vessels")
+ImGui.AddTab("RRestoredMonstersTabs", "RRestoredMonstersTabVessel", "Vessels")
 
 -- Vessel type
-ImGui.AddCombobox("tcRestoredMonstersTabVessel", "tcRestoredMonstersTabVesselType", "Vessel type", 
+ImGui.AddCombobox("RRestoredMonstersTabVessel", "RRestoredMonstersTabVesselType", "Vessel type", 
 function(index, str)
     mod.vesselType = index + 1
 end, {"Normal", "Legacy"}, 0, true)
-ImGui.SetHelpmarker("tcRestoredMonstersTabVesselType", "Replaces vessels with their legacy version.\nDisabled by default.")
+ImGui.SetHelpmarker("RRestoredMonstersTabVesselType", "Replaces vessels with their legacy version.\nDisabled by default.")
 
 -- Echo bats tab
-ImGui.AddTab("tcRestoredMonstersTabs", "tcRestoredMonstersTabBlindBat", "Echo bats")
+ImGui.AddTab("RRestoredMonstersTabs", "RRestoredMonstersTabBlindBat", "Echo bats")
 
-ImGui.AddSliderInteger("tcRestoredMonstersTabBlindBat", "tcRestoredMonstersTabBlindBatScream", "Scream effect",
+ImGui.AddSliderInteger("RRestoredMonstersTabBlindBat", "RRestoredMonstersTabBlindBatScream", "Scream effect",
 function(val)
     mod.blindBatScreamInc = val
 end, 3, 1, 5)
 
-ImGui.SetHelpmarker("tcRestoredMonstersTabBlindBatScream", "Changes how strong the blind bat effect is.\nAt 3 by default.")
+ImGui.SetHelpmarker("RRestoredMonstersTabBlindBatScream", "Changes how strong the blind bat effect is.\nAt 3 by default.")
 
-ImGui.AddCallback("tcRestoredMonstersWindow", ImGuiCallback.Render, function()
-    ImGui.UpdateData("tcRestoredMonstersTabVesselType", ImGuiData.Value, mod.vesselType - 1)
-    ImGui.UpdateData("tcRestoredMonstersTabBlindBatScream", ImGuiData.Value, mod.blindBatScreamInc)
+ImGui.AddCallback("RRestoredMonstersWindow", ImGuiCallback.Render, function()
+    ImGui.UpdateData("RRestoredMonstersTabVesselType", ImGuiData.Value, mod.vesselType - 1)
+    ImGui.UpdateData("RRestoredMonstersTabBlindBatScream", ImGuiData.Value, mod.blindBatScreamInc)
 end)
